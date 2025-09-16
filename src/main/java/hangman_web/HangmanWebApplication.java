01154a45c65a096dd8330f148d25d3ae51ffd9c3
dramaticLemon <chebandima27@gmail.com>
2025-09-16 14:09:55 +0300

    initial project

---
+++ src/main/java/hangman_web/HangmanWebApplication.java
@@ -0,0 +1,13 @@
+package hangman_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmanWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangmanWebApplication.class, args);
+	}
+
+}
